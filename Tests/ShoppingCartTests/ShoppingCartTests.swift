e04314b9e6a4f5eb48586bd3a6d049711e850a18
Daniel <daniel@danramteke.com>
2019-07-01 19:02:54 -0400

    step 2

--- Tests/ShoppingCartTests/ShoppingCartTests.swift
+++ Tests/ShoppingCartTests/ShoppingCartTests.swift
@@ -4,6 +4,7 @@ import XCTest
 final class ShoppingCartTests: XCTestCase {
     func testAddProductsToTheShoppingCart() {
         // Given: 
+
         // An empty shopping cart
         var shoppingCart = ShoppingCart()
 
@@ -11,10 +12,12 @@ final class ShoppingCartTests: XCTestCase {
         let product = Product(name: "Dove Soap", unitPrice: 39.99)
 
         // When:
+
         // The user adds 5 Dove Soaps to the shopping cart
         shoppingCart.add(product, quantity: 5)
 
         // Then:
+
         // The shopping cart should contain 5 Dove Soaps each with a unit price of 39.99
         XCTAssertEqual(shoppingCart.products, [
             Product(name: "Dove Soap", unitPrice: 39.99), 
@@ -27,7 +30,40 @@ final class ShoppingCartTests: XCTestCase {
         XCTAssertEqual(shoppingCart.totalPrice, 199.95)
     }
 
+    func testAddAdditionalProductsOfTheSameTypeToTheShoppingCart() {
+        // Given:
+
+        // An empty shopping cart
+        var shoppingCart = ShoppingCart()
+        // And a product, Dove Soap with a unit price of 39.99
+        let product = Product(name: "Dove Soap", unitPrice: 39.99)
+
+        // When:
+
+        // The user adds 5 Dove Soaps to the shopping cart
+        shoppingCart.add(product, quantity: 5)
+        // And then adds another 3 Dove Soaps to the shopping cart
+        shoppingCart.add(product, quantity: 3)
+
+        // Then:
+
+        // The shopping cart should contain 8 Dove Soaps each with a unit price of 39.99
+        XCTAssertEqual(shoppingCart.products, [
+            Product(name: "Dove Soap", unitPrice: 39.99),
+            Product(name: "Dove Soap", unitPrice: 39.99),
+            Product(name: "Dove Soap", unitPrice: 39.99),
+            Product(name: "Dove Soap", unitPrice: 39.99), 
+            Product(name: "Dove Soap", unitPrice: 39.99), 
+            Product(name: "Dove Soap", unitPrice: 39.99), 
+            Product(name: "Dove Soap", unitPrice: 39.99), 
+            Product(name: "Dove Soap", unitPrice: 39.99)
+        ])
+        // And the shopping cart’s total price should equal 319.92
+        XCTAssertEqual(shoppingCart.totalPrice, Decimal(319.92).rounded)
+    }
+
     static var allTests = [
         ("testAddProductsToTheShoppingCart", testAddProductsToTheShoppingCart),
+        ("testAddAdditionalProductsOfTheSameTypeToTheShoppingCart", testAddAdditionalProductsOfTheSameTypeToTheShoppingCart),
     ]
 }
